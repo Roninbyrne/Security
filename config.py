7f8faf75bcfb94211f57f3f781275bfb4d7f5c0b
Ronin <bhabyaprakash01@gmail.com>
2025-01-11 19:54:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ UPSTREAM_REPO = getenv(
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
-    "GIT_TOKEN", None
+    "   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", None
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/PacificArc")
