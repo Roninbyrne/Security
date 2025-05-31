6648699d8cbe983dc38fa188a8365608f061e873
Ronin <bhabyaprakash01@gmail.com>
2025-05-31 14:06:07 +0530

    Update werewolf_command.py

--- Security/plugins/tools/werewolf_command.py
+++ Security/plugins/tools/werewolf_command.py
@@ -4,7 +4,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.enums import ParseMode
 from plugins.tools.db import games_col, players_col
 from Security.plugins.tools.config import JOIN_TIME, MIN_PLAYERS, MAX_PLAYERS
-from plugins.tools.werewolf_core import generate_roles, reset_game, day_night_cycle
+from Security.plugins.tools.werewolf_core import generate_roles, reset_game, day_night_cycle
 from datetime import datetime
 import asyncio
 
